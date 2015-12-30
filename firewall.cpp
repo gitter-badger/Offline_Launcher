8f151f323717fff18349ddbeb979592478a2014b
BlaCGaming <BlaCGaming@users.noreply.github.com>
2015-12-30 15:51:52 +0100

    Firewall rules
    add/remove

--- firewall.cpp
+++ firewall.cpp
@@ -15,7 +15,12 @@ void Firewall::block()
     QString filename = program->getFileName();
     QFileInfo fi(filename);
     QString name = fi.baseName();  // base = "archive"
-    QString command = QString("netsh advfirewall firewall add rule name=\"temp%1\""
+    QString command = QString("netsh advfirewall firewall delete rule name=\"temp%1\""
+                              " program=\"%2\"").arg(name).arg(filename.replace("/", "\\"));
+
+    QProcess::execute(command);
+
+    command = QString("netsh advfirewall firewall add rule name=\"temp%1\""
                               " dir=in action=block program=\"%2\" enable=yes").arg(name).arg(filename.replace("/", "\\"));
 
     QProcess process;
@@ -34,7 +39,13 @@ void Firewall::unblock()
     QString filename = program->getFileName();
     QFileInfo fi(filename);
     QString name = fi.baseName();  // base = "archive"
-    QString command = QString("netsh advfirewall firewall add rule name=\"temp%1\""
+
+    QString command = QString("netsh advfirewall firewall delete rule name=\"temp%1\""
+                              " program=\"%2\"").arg(name).arg(filename.replace("/", "\\"));
+
+    QProcess::execute(command);
+
+    command = QString("netsh advfirewall firewall add rule name=\"temp%1\""
                               " dir=in action=allow program=\"%2\" enable=yes").arg(name).arg(filename.replace("/", "\\"));
     qDebug() << command;
     QProcess process;
